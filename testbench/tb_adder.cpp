4bb17caf566a0d0b73c8eaea05cd9f57a3019572
aitesam961 <aitesam961@gmail.com>
2023-12-08 10:04:32 +0500

    Create tb_adder.cpp

---
+++ testbench/tb_adder.cpp
@@ -0,0 +1,14 @@
+#include<stdlib>
+#include<iostream>
+#include<verilated.h>
+#include<verilated_vcd_c.h>
+#include<Vadder.h>
+#include<Vadder__024unit.h>
+
+#define SIM_TIME_MAX    20
+vluint64_t sim_time     =   0;
+
+
+int main(int argc, char** argv, char** env){
+    
+}
